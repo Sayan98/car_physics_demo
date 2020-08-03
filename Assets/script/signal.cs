50228b858f34c6a7a6672057582c96f159027f49
Sayan Chanda <mysayanchanda@gmail.com>
2020-08-03 22:12:12 +0530

    added traffic
    added traffic, signal, cars stop at signal and when a car is infront of another.
    (bug)- player car is passing through traffic cars.

---
+++ Assets/script/signal.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class signal : MonoBehaviour
+{
+    
+    MeshRenderer renderer;
+
+    void Awake() {
+        
+        renderer = GetComponent<MeshRenderer>();
+
+    }
+    void Start() {
+
+        var choice = Random.Range(0,2);
+        
+        if(choice == 0)
+            renderer.material.color = Color.red;
+        else if(choice == 1)
+                renderer.material.color = Color.yellow;
+        else if(choice == 2)
+                renderer.material.color = Color.green;
+
+        StartCoroutine(change_color());
+    
+    }
+
+
+    IEnumerator change_color() {
+
+        yield return new WaitForSeconds(5);
+
+        if(renderer.material.color == Color.red)
+            renderer.material.color = Color.yellow;
+
+        else if(renderer.material.color == Color.yellow)
+                renderer.material.color = Color.green;
+        else 
+            renderer.material.color = Color.red;
+
+        StartCoroutine(change_color());
+
+    }
+    
+}
---
+++ Assets/script/traffic_cars.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+
+public class traffic_cars : MonoBehaviour
+{
+    
+    [SerializeField]
+    PathCreator path;
+
+    public float speed, local_speed;
+
+    public float distance;
+
+
+    void Start() {
+        local_speed = speed;
+    }
+    
+    
+    void Update() {
+        
+        distance += local_speed * Time.deltaTime;
+        transform.position = path.path.GetPointAtDistance(distance);
+        transform.rotation = path.path.GetRotationAtDistance(distance);
+
+    }
+    
+    void OnTriggerStay(Collider enter) {
+
+        if(enter.gameObject.tag == "signal") {
+
+            MeshRenderer renderer = enter.gameObject.GetComponent<MeshRenderer>();
+            
+            if(renderer.material.color == Color.red || renderer.material.color == Color.yellow)
+                local_speed = 0;
+            else
+                local_speed = speed;
+        }
+
+    }
+
+
+    void OnCollisionStay(Collision enter) {
+        
+        if(enter.gameObject.tag == "cars") {
+
+            Vector3 forward = transform.TransformDirection(Vector3.forward);
+            Vector3 toOther = enter.gameObject.transform.position - transform.position;
+
+            if (Vector3.Dot(forward, toOther) > 0) {
+
+                local_speed = 0;
+            
+            }
+
+        }
+
+    }
+
+
+    void OnCollisionExit(Collision exit) {
+        
+        if(exit.gameObject.tag == "cars")
+            StartCoroutine(wait_start());
+
+    }
+
+
+    IEnumerator wait_start() {
+
+        yield return new WaitForSeconds(1.5f);
+        local_speed = speed;
+
+    }
+
+}
